bb131e0bc75ff42ae883bc7b5dcc371ff40e154e
vaden89 <isaacshosanya89@gmail.com>
2026-02-19 13:23:03 +0100

    feat: built out an interactive poems display section

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-console.log("loaded");
+let poems = [];
+let currentPoemIndex = 0;
 
 const stackingHeartsContainer = document.getElementById("stacking-hearts");
+const poemContainer = document.getElementById("poem-container");
+const previousPoemBtn = document.getElementById("previous-poem-btn");
+const nextPoemBtn = document.getElementById("next-poem-btn");
+
+previousPoemBtn.addEventListener("click", () => {
+  if (currentPoemIndex == 0) return;
+
+  currentPoemIndex--;
+  poemContainer.innerText = poems[currentPoemIndex];
+});
+
+nextPoemBtn.addEventListener("click", () => {
+  if (currentPoemIndex == poems.length - 1) return;
+
+  currentPoemIndex++;
+  poemContainer.innerText = poems[currentPoemIndex];
+});
+
+const fetchPoems = () => {
+  fetch("/data/poems.json")
+    .then((res) => res.json())
+    .then((data) => {
+      poems = data.lovePoems;
+
+      console.log(poems);
+      poemContainer.innerText = poems[currentPoemIndex];
+    })
+    .catch((error) => console.log(error));
+};
+
+fetchPoems();
